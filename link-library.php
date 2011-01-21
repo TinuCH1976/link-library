b06f72e9b87df1a6e4af181a25048c0d2e226bda
jackdewey <jackdewey@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-21 19:40:27 +0000

    Corrected typo
    git-svn-id: https://plugins.svn.wordpress.org/link-library/trunk@335607 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- link-library.php
+++ link-library.php
@@ -1484,7 +1484,7 @@ class link_library_plugin {
 				</table>
 			</td>
 			<td style='padding: 8px; border: 1px solid #cccccc;'>
-			<div><h3>ThemeFuse Original WP Themes</h3><br />If you are looking to buy an original WP theme, take a look at <a href="https://www.e-junkie.com/ecom/gb.php?cl=136641&c=ib&aff=153522" target="ejejcsingle">ThemeFuse</a>?<br />They have a nice 1-click installer, great support and good-looking themes.</div><div style='text-align: center; padding-top: 10px'><a href="https://www.e-junkie.com/ecom/gb.php?cl=136641&c=ib&aff=153522" target="ejejcsingle"><img src='http://themefuse.com/wp-content/themes/themefuse/images/campaigns/themefuse.jpg' /></a></div>
+			<div><h3>ThemeFuse Original WP Themes</h3><br />If you are looking to buy an original WP theme, take a look at <a href="https://www.e-junkie.com/ecom/gb.php?cl=136641&c=ib&aff=153522" target="ejejcsingle">ThemeFuse</a><br />They have a nice 1-click installer, great support and good-looking themes.</div><div style='text-align: center; padding-top: 10px'><a href="https://www.e-junkie.com/ecom/gb.php?cl=136641&c=ib&aff=153522" target="ejejcsingle"><img src='http://themefuse.com/wp-content/themes/themefuse/images/campaigns/themefuse.jpg' /></a></div>
 			</td>
 			</table>
 		<?php }
