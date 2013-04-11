52c4835078866eb32d5975c003453d1dc6032cc7
jackdewey <jackdewey@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-11 00:28:11 +0000

    Forgot to bump up version number.
    git-svn-id: https://plugins.svn.wordpress.org/link-library/trunk@695607 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- link-library.php
+++ link-library.php
@@ -3,7 +3,7 @@
 Plugin Name: Link Library
 Plugin URI: http://wordpress.org/extend/plugins/link-library/
 Description: Display links on pages with a variety of options
-Version: 5.7.9
+Version: 5.7.9.1
 Author: Yannick Lefebvre
 Author URI: http://yannickcorner.nayanna.biz/
 
