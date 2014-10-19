9782238a9db8cd9c9c0b6f4bac3426a4c7e945cb
jackdewey <jackdewey@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-19 16:18:47 +0000

    Forgot to update version number in main code file
    git-svn-id: https://plugins.svn.wordpress.org/link-library/trunk@1010030 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- link-library.php
+++ link-library.php
@@ -3,7 +3,7 @@
 Plugin Name: Link Library
 Plugin URI: http://wordpress.org/extend/plugins/link-library/
 Description: Display links on pages with a variety of options
-Version: 5.8.10.4
+Version: 5.8.10.5
 Author: Yannick Lefebvre
 Author URI: http://ylefebvre.ca/
 
